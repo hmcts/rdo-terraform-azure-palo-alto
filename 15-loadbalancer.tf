dd6aaca0f9062dec07e1d4a41cb57884b0a37842
stravze <stravze@gmail.com>
2019-06-24 15:20:17 +0100

    testing load balancer id setup

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -12,7 +12,7 @@ resource "azurerm_lb" "lb" {
 resource "azurerm_lb_backend_address_pool" "lb_backend" {
   name                                              = "lbb-dmz-firewall"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-  loadbalancer_id                                   = "${element(azurerm_lb.lb.*.id, count.index)}"
+  loadbalancer_id                                   = "${azurerm_lb.lb.*.id}"   #"${element(azurerm_lb.lb.*.id, count.index)}"
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbmap" {
