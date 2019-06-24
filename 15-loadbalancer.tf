fe056b44b94b4bda5689a35803db7afa2c08bcb8
stravze <stravze@gmail.com>
2019-06-24 15:24:06 +0100

    count reference

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -1,8 +1,9 @@
 resource "azurerm_lb" "lb" {
-  name                                              = "lb-dmz-firewall"
+  name                                              = "lb-dmz-firewall-${count.index"
   location                                          = "${azurerm_resource_group.rg_firewall.location}"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
   sku                                               = "Standard"
+  count                                             = "${var.replicas}"
   frontend_ip_configuration {
     name                                            = "frontend"
     subnet_id                                       = "${var.subnet_transit_public_id}"
@@ -12,7 +13,7 @@ resource "azurerm_lb" "lb" {
 resource "azurerm_lb_backend_address_pool" "lb_backend" {
   name                                              = "lbb-dmz-firewall"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-  loadbalancer_id                                   = "${azurerm_lb.lb.*.id}"   #"${element(azurerm_lb.lb.*.id, count.index)}"
+  loadbalancer_id                                   = "${element(azurerm_lb.lb.*.id, count.index)}"
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbmap" {
