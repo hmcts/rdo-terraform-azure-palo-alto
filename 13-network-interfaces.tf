6bd9af584ce23a3e1a34fb79d5f15724c5f19d75
stravze <stravze@gmail.com>
2019-06-24 13:35:19 +0100

    fixing count

--- 13-network-interfaces.tf
+++ 13-network-interfaces.tf
@@ -1,10 +1,10 @@
 resource "azurerm_network_interface" "nic_mgmt" {
-  name                                              = "firewall-${var.environment}-nic-mgmt-${count.index}"
+  name                                              = "firewall-${var.environment}-nic-mgmt"
   location                                          = "${azurerm_resource_group.rg_firewall.location}"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
 
   ip_configuration {
-    name                                            = "firewall-${var.environment}-nic-mgmt-ip-${count.index}"
+    name                                            = "firewall-${var.environment}-nic-mgmt-ip"
     subnet_id                                       = "${var.subnet_management_id}"
     private_ip_address_allocation                   = "dynamic"
   }
