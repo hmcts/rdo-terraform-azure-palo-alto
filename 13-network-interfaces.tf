2e9ab4e5de79d01a0dec3cf831f07676ea06d339
stravze <stravze@gmail.com>
2019-06-24 13:51:54 +0100

    count issue - testing

--- 13-network-interfaces.tf
+++ 13-network-interfaces.tf
@@ -12,14 +12,14 @@ resource "azurerm_network_interface" "nic_mgmt" {
 }
 
 resource "azurerm_network_interface" "nic_transit_public" {
-  name                                              = "firewall-${var.environment}-nic-transit-public"
+  name                                              = "firewall-${var.environment}-nic-transit-public-${count.index}"
   location                                          = "${azurerm_resource_group.rg_firewall.location}"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
   count                                             = "${var.replicas}"
   enable_ip_forwarding                              = "true"
 
   ip_configuration {
-    name                                            = "${azurerm_network_interface.nic_transit_public.name}"
+    name                                            = "${azurerm_network_interface.nic_transit_public.name}-${count.index}"
     subnet_id                                       = "${var.subnet_transit_id}"
     private_ip_address_allocation                   = "dynamic"
   }
@@ -27,14 +27,14 @@ resource "azurerm_network_interface" "nic_transit_public" {
 
 
 resource "azurerm_network_interface" "nic_transit_private" {
-  name                                              = "firewall-${var.environment}-nic-transit-private"
+  name                                              = "firewall-${var.environment}-nic-transit-private-${count.index}"
   location                                          = "${azurerm_resource_group.rg_firewall.location}"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
   count                                             = "${var.replicas}"
   enable_ip_forwarding                              = "true"
 
   ip_configuration {
-    name                                            = "${azurerm_network_interface.nic_transit_private.name}"
+    name                                            = "${azurerm_network_interface.nic_transit_private.name}-${count.index}"
     subnet_id                                       = "${var.subnet_transit_id}"
     private_ip_address_allocation                   = "dynamic"
   }
