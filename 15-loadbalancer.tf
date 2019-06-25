af3b8ec7c6e77d234f697aa083cf0eb4746b0bb6
stravze <stravze@gmail.com>
2019-06-25 11:47:45 +0100

    load balancer fixes

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -18,7 +18,7 @@ resource "azurerm_lb_backend_address_pool" "lb_backend" {
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbmap" {
   count                                             = "${var.replicas}"
-  network_interface_id                              = "${element(azurerm_network_interface.nic_transit_public.*.id, count.index)}"
+  network_interface_id                              = "${azurerm_network_interface.nic_transit_public}"
   ip_configuration_name                             = "firewall-${var.environment}-transit-public-${count.index}"
   backend_address_pool_id                           = "${azurerm_lb_backend_address_pool.lb_backend.id}"
 }
