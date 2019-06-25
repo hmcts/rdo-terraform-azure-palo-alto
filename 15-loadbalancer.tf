f3b2a4f5bd8c8ee8408d5375565e70cf3ba8a54a
stravze <stravze@gmail.com>
2019-06-25 10:29:41 +0100

    more load balaner fixes

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -13,19 +13,19 @@ resource "azurerm_lb" "lb" {
 resource "azurerm_lb_backend_address_pool" "lb_backend" {
   name                                              = "firewall-${var.environment}-lb-backend"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-  loadbalancer_id                                   = "${azurerm_lb.lb.*.id}"  #"${element(azurerm_lb.lb.*.id, count.index)}"
+  loadbalancer_id                                   = "${azurerm_lb.lb.id}"  #"${element(azurerm_lb.lb.*.id, count.index)}"
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbmap" {
   count                                             = "${var.replicas}"
   network_interface_id                              = "${element(azurerm_network_interface.nic_transit_public.*.id, count.index)}"
   ip_configuration_name                             = "firewall-${var.environment}-transit-public-${count.index}"
-  backend_address_pool_id                           = "${azurerm_lb_backend_address_pool.lb_backend.*.id}"
+  backend_address_pool_id                           = "${azurerm_lb_backend_address_pool.lb_backend.id}"
 }
 
 resource "azurerm_lb_probe" "lb_probe" {
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-  loadbalancer_id                                   = "${azurerm_lb.lb.*.id}"  #"${element(azurerm_lb.lb.*.id, count.index)}"
+  loadbalancer_id                                   = "${azurerm_lb.lb.id}"  #"${element(azurerm_lb.lb.*.id, count.index)}"
   name                                              = "probe-https"
   port                                              = "443"
   protocol                                          = "Tcp"
@@ -33,7 +33,7 @@ resource "azurerm_lb_probe" "lb_probe" {
 
 resource "azurerm_lb_rule" "lb_rule" {
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-  loadbalancer_id                                   = "${azurerm_lb.lb.*.id}"  #"${element(azurerm_lb.lb.*.id, count.index)}"
+  loadbalancer_id                                   = "${azurerm_lb.lb.id}"  #"${element(azurerm_lb.lb.*.id, count.index)}"
   name                                              = "firewall-${var.environment}-lbrules"
   frontend_port                                     = "0"
   frontend_ip_configuration_name                    = "frontend"
