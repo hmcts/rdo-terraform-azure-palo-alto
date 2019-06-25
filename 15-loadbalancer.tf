cd16ca0763fdc603904bfc3ff6411e7ac1e09771
stravze <stravze@gmail.com>
2019-06-25 13:03:41 +0100

    lb

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -20,7 +20,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "lbmap" {
   count                                             = "${var.replicas}"
   network_interface_id                              = "${element(azurerm_network_interface.nic_transit_public.*.id, count.index)}"
   ip_configuration_name                             = "firewall-${var.environment}-transit-public-${count.index}"
-  backend_address_pool_id                           = "${azurerm_lb_backend_address_pool.lb_backend.*.id}"
+  backend_address_pool_id                           = ["${azurerm_lb_backend_address_pool.lb_backend.*.id}"]
 }
 
 resource "azurerm_lb_probe" "lb_probe" {
