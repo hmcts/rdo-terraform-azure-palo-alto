ff12bf1ed670430a121320162d405f00961d55a6
stravze <stravze@gmail.com>
2019-06-26 13:17:28 +0100

    sku issues

--- 12-availability-set.tf
+++ 12-availability-set.tf
@@ -1,8 +1,8 @@
-#resource "azurerm_availability_set" "availability_set" {
-#  name                                              = "fw-${var.environment}-avs"
-#  resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-#  location                                          = "${azurerm_resource_group.rg_firewall.location}"
-#  platform_update_domain_count                      = "${var.avail_update_domains}"
-#  platform_fault_domain_count                       = "${var.avail_fault_domains}"
-#  managed                                           = "${var.avail_managed}"
-#}
+resource "azurerm_availability_set" "availability_set" {
+  name                                              = "fw-${var.environment}-avs"
+  resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
+  location                                          = "${azurerm_resource_group.rg_firewall.location}"
+  platform_update_domain_count                      = "${var.avail_update_domains}"
+  platform_fault_domain_count                       = "${var.avail_fault_domains}"
+  managed                                           = "${var.avail_managed}"
+}
