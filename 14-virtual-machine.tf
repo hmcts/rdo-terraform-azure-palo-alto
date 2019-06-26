ca469d741ee4fe768ca3a330e0cfbf0dd77cd150
stravze <stravze@gmail.com>
2019-06-26 09:13:57 +0100

    testing public ip address

--- 14-virtual-machine.tf
+++ 14-virtual-machine.tf
@@ -4,6 +4,7 @@ resource "azurerm_virtual_machine" "pan_vm" {
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
   availability_set_id                               = "${azurerm_availability_set.availability_set.id}"
   vm_size                                           = "${var.vm_size}"
+  delete_os_disk_on_termination                     = true
   count                                             = "${var.replicas}"
 
   plan {
--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -2,7 +2,7 @@ resource "azurerm_lb" "lb" {
   name                                              = "firewall-${var.environment}-lb-${count.index}"
   location                                          = "${azurerm_resource_group.rg_firewall.location}"
   resource_group_name                               = "${azurerm_resource_group.rg_firewall.name}"
-  sku                                               = "Standard"
+  sku                                               = "basic"
   count                                             = "${var.replicas}"
   frontend_ip_configuration {
     name                                            = "frontend"
