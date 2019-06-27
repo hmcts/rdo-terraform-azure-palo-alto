651e1c00baab744a90298a9a9fb35c3f4dda02af
stravze <stravze@gmail.com>
2019-06-27 14:10:27 +0100

    testing nic issue

--- 16-ansible-master.tf
+++ 16-ansible-master.tf
@@ -103,7 +103,7 @@ resource "azurerm_network_interface" "ansible_server_nic" {
 
     ip_configuration {
         name                                                = "fw-${var.environment}-ansible-ip"
-        subnet_id                                           = "${var.subnet_management_id}"
+        subnet_id                                           = "${element(azurerm_subnet.subnet.*.id, index(azurerm_subnet.subnet.*.name, var.subnet_management_id))}"   #"${var.subnet_management_id}"
         private_ip_address_allocation                       = "dynamic"
         public_ip_address_id                                = "${azurerm_public_ip.pip-ansible.id}"
     }
