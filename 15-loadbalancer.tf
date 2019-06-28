ce45b026e1238fb5325fa0c80bf63accd9205747
stravze <stravze@gmail.com>
2019-06-28 14:25:25 +0100

    changing protocol for lb rule

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -41,7 +41,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   frontend_ip_configuration_name                    = "frontend"
   backend_address_pool_id                           = "${azurerm_lb_backend_address_pool.lb_backend.id}"
   backend_port                                      = "80"  ##
-  protocol                                          = "All"
+  protocol                                          = "tcp"
   enable_floating_ip                                = "true"
   probe_id                                          = "${azurerm_lb_probe.lb_probe.id}"
 }
