60ab7d57620a074c2a108aca0b9cd9dde1e8c676
stravze <stravze@gmail.com>
2019-07-01 11:59:59 +0100

    fixing public ip address issues

--- 16-ansible-master.tf
+++ 16-ansible-master.tf
@@ -9,7 +9,7 @@ data "template_file" "inventory" {
     vars = {
         admin_username                                      = "${var.vm_username}"
         admin_password                                      = "${var.vm_password}"
-        public_ip                                           = "${join("\n", azurerm_network_interface.nic_mgmt.*.private_ip_address)}"
+        public_ip                                           = "${join("\n", azurerm_public_ip.palo_public_ip.ip_address)}"
     }
 }
 
