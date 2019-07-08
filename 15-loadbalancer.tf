36fa63d66b8f8494324d2eb21789e0209ea1d5d2
stravze <stravze@gmail.com>
2019-07-08 09:14:37 +0100

    testing address pool

--- 15-loadbalancer.tf
+++ 15-loadbalancer.tf
@@ -39,7 +39,7 @@ resource "azurerm_lb_rule" "lb_rule" {
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbmap" {
   count                                             = "${var.replicas}"
-  network_interface_id                              = "${element(azurerm_network_interface.nic_transit_public.*.id, count.index)}"
+  network_interface_id                              = "${element(azurerm_network_interface.nic_transit_public.0.id, count.index)}"
   ip_configuration_name                             = "fw-${var.environment}-nic-transit-public-ip-${count.index}"
   backend_address_pool_id                           = "${azurerm_lb_backend_address_pool.lb_backend.id}"
 }
