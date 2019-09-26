0fcc785d1a890f6bf7df7be62652563cb2beba14
gunnertwin <daniel@alvestechnology.io>
2019-09-26 10:07:22 +0100

    Replace static ip entries with variables

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -1,5 +1,6 @@
 locals {
   default_gateway                                   = "${cidrhost(data.azurerm_subnet.subnet.address_prefix,1)}"
+  default_gateway_trust                             = "${cidrhost(data.azurerm_subnet.subnet_trust.address_prefix,1)}"
 }
 
 data "azurerm_subnet" "subnet" {
@@ -9,6 +10,12 @@ data "azurerm_subnet" "subnet" {
   depends_on                                        = ["azurerm_public_ip.palo_mgmt_ip"]
 }
 
+data "azurerm_subnet" "subnet_trust" {
+  name                                              = "sub-hub-transit-private"
+  virtual_network_name                              = "hub"
+  resource_group_name                               = "hub"
+  depends_on                                        = ["azurerm_public_ip.palo_mgmt_ip"]
+}
 
 data "template_file" "inventory" {
     template                                                = "${file("${path.module}/templates/inventory.tpl")}"
