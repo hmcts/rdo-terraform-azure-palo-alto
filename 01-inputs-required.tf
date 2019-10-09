c7189314fecbdf56c42a08c9dcb77d944aa839fb
stravze <stravze@gmail.com>
2019-10-09 09:47:38 +0100

    adding proxy subnet lookup

--- 01-inputs-required.tf
+++ 01-inputs-required.tf
@@ -42,3 +42,6 @@ variable "ansible-nic" {
   description                                       = "Ansible Nic"
 }
 
+variable "proxy_subnet" {
+  description                                       = "Ansible Nic"
+}
