75734a6cefb5289d8c12496d76ee33ca52402af2
Daniel <daniel@alvestechnology.io>
2020-03-05 12:19:23 +0000

    add quotes around variable

--- 16-ansible-master.tf
+++ 16-ansible-master.tf
@@ -1,58 +1,58 @@
 resource "null_resource" "update_inventory" {
 
-    triggers = {
-        template                                            = "${data.template_file.inventory.rendered}"
-    }
+  triggers = {
+    template = "${data.template_file.inventory.rendered}"
+  }
 
-    provisioner "local-exec" {
-        command                                             = "echo '${data.template_file.inventory.rendered}' > ${path.module}/ansible/inventory"
-    }
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.inventory.rendered}' > ${path.module}/ansible/inventory"
+  }
 }
 
 resource "azurerm_virtual_machine" "ansible-host" {
-  name                                                      = "${var.vnet_name}-palo-ansible"
-  location                                                  = "${azurerm_resource_group.rg_firewall.location}"
-  resource_group_name                                       = "${azurerm_resource_group.rg_firewall.name}"
-  network_interface_ids                                     = ["${var.ansible-nic}"]
-  vm_size                                                   = "Standard_A1_v2"
-  delete_os_disk_on_termination                             = true
-  
+  name                          = "${var.vnet_name}-palo-ansible"
+  location                      = "${azurerm_resource_group.rg_firewall.location}"
+  resource_group_name           = "${azurerm_resource_group.rg_firewall.name}"
+  network_interface_ids         = ["${var.ansible-nic}"]
+  vm_size                       = "Standard_A1_v2"
+  delete_os_disk_on_termination = true
+
   storage_image_reference {
-    publisher                                               = "Canonical"
-    offer                                                   = "UbuntuServer"
-    sku                                                     = "18.04-LTS"
-    version                                                 = "latest"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
-  
+
   storage_os_disk {
-    name                                                    = "${var.vnet_name}-palo-ansible-os"
-    caching                                                 = "ReadWrite"
-    create_option                                           = "FromImage"
-    managed_disk_type                                       = "Standard_LRS"
+    name              = "${var.vnet_name}-palo-ansible-os"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
-  
+
   os_profile {
-    computer_name                                           = "${var.vnet_name}-palo-ans-os"
-    admin_username                                          = "${var.vm_username}"
-    admin_password                                          = "${var.vm_password}"
+    computer_name  = "${var.vnet_name}-palo-ans-os"
+    admin_username = "${var.vm_username}"
+    admin_password = "${var.vm_password}"
   }
 
 
-provisioner "remote-exec" {
-    inline                                                  = [
-                                                                "mkdir ~/ansible"
-                                                            ]
-      connection {
-    type                                                    = "ssh"
-    user                                                    = "${var.vm_username}"
-    password                                                = "${var.vm_password}"
-    host                                                    = "${var.pip-ansible}"
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p ~/ansible"
+    ]
+    connection {
+      type     = "ssh"
+      user     = "${var.vm_username}"
+      password = "${var.vm_password}"
+      host     = "${var.pip-ansible}"
 
- }
-}
+    }
+  }
 
   os_profile_linux_config {
-    disable_password_authentication                         = false
+    disable_password_authentication = false
   }
 
 
@@ -60,15 +60,15 @@ provisioner "remote-exec" {
 
 
 resource "azurerm_virtual_machine_extension" "ansible_extension" {
-  name                                                      = "Ansible-Agent-Install"
-  location                                                  = "${azurerm_resource_group.rg_firewall.location}"
-  resource_group_name                                       = "${azurerm_resource_group.rg_firewall.name}"
-  virtual_machine_name                                      = "${azurerm_virtual_machine.ansible-host.name}"
-  publisher                                                 = "Microsoft.Azure.Extensions"
-  type                                                      = "CustomScript"
-  type_handler_version                                      = "2.0"
+  name                 = "Ansible-Agent-Install"
+  location             = "${azurerm_resource_group.rg_firewall.location}"
+  resource_group_name  = "${azurerm_resource_group.rg_firewall.name}"
+  virtual_machine_name = "${azurerm_virtual_machine.ansible-host.name}"
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
 
-  settings                                                  = <<SETTINGS
+  settings = <<SETTINGS
                                                                 {
                                                                     "commandToExecute": "sudo apt-add-repository --yes --update ppa:ansible/ansible",
                                                                     "commandToExecute": "sudo apt-get update && sudo apt install -y software-properties-common ansible libssl-dev libffi-dev python-dev python-pip && sudo pip install pywinrm && sudo pip install azure-keyvault && sudo pip install requests && sudo pip install requests-toolbelt"
@@ -78,40 +78,40 @@ resource "azurerm_virtual_machine_extension" "ansible_extension" {
 
 
 resource "null_resource" "ansible-runs" {
-    triggers                                                = {
-      always_run                                            = "${timestamp()}"
-    }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 
-    depends_on                                              = [
-                                                                "azurerm_virtual_machine.pan_vm",
-                                                                "azurerm_virtual_machine_extension.ansible_extension",
-                                                                "azurerm_virtual_machine.ansible-host"
-                                                            ]
+  depends_on = [
+    "azurerm_virtual_machine.pan_vm",
+    "azurerm_virtual_machine_extension.ansible_extension",
+    "azurerm_virtual_machine.ansible-host"
+  ]
 
   provisioner "file" {
-    source                                                  = "${path.module}/ansible/"
-    destination                                             = "~/ansible/"
-  
+    source      = "${path.module}/ansible/"
+    destination = "~/ansible/"
+
     connection {
-      type                                                  = "ssh"
-      user                                                  = "${var.vm_username}"
-      password                                              = "${var.vm_password}"
-      host                                                  = "${var.pip-ansible}"
+      type     = "ssh"
+      user     = "${var.vm_username}"
+      password = "${var.vm_password}"
+      host     = "${var.pip-ansible}"
     }
   }
 
   provisioner "remote-exec" {
-    inline                                                  = [
-                                                                "sudo apt-get install sshpass",
-                                                                "ansible-galaxy install -f PaloAltoNetworks.paloaltonetworks",
-                                                                "ansible-playbook -i /home/localadmin/ansible/inventory -vvvvvvv /home/localadmin/ansible/palo.yml -e mgmt_ip=${azurerm_public_ip.palo_mgmt_ip.ip_address} -e admin_username=${var.vm_username} -e admin_password=${var.vm_password} -e paloname=${azurerm_virtual_machine.pan_vm.0.name} -e gateway=${local.default_gateway} -e gateway_trust=${local.default_gateway_trust} -e gateway_dmz=${local.default_gateway_dmz} -e gateway_dmz_trust=${local.default_gateway_dmz_trust} -e f5_data_subnet=${local.f5_data_subnet} -e env_subscripton=${var.environment} -e globalscape_pub_key=${var.globalscape_pub_key} -e globalscape_priv_key=${var.globalscape_priv_key} -e prod_cert_pub_key=${var.prod_cert_pub_key} -e prod_cert_priv_key=${var.prod_cert_priv_key}",
-                                                            ]
-                                                         
+    inline = [
+      "sudo apt-get install sshpass",
+      "ansible-galaxy install -f PaloAltoNetworks.paloaltonetworks",
+      "ansible-playbook -i /home/localadmin/ansible/inventory -vvvvvvv /home/localadmin/ansible/palo.yml -e mgmt_ip=${azurerm_public_ip.palo_mgmt_ip.ip_address} -e admin_username=${var.vm_username} -e admin_password=${var.vm_password} -e paloname=${azurerm_virtual_machine.pan_vm.0.name} -e gateway=${local.default_gateway} -e gateway_trust=${local.default_gateway_trust} -e gateway_dmz=${local.default_gateway_dmz} -e gateway_dmz_trust=${local.default_gateway_dmz_trust} -e f5_data_subnet=${local.f5_data_subnet} -e env_subscripton=${var.environment} -e globalscape_pub_key='${var.globalscape_pub_key}' -e 'globalscape_priv_key=${var.globalscape_priv_key}' -e 'prod_cert_pub_key=${var.prod_cert_pub_key}' -e 'prod_cert_priv_key=${var.prod_cert_priv_key}'",
+    ]
+
     connection {
-      type                                                  = "ssh"
-      user                                                  = "${var.vm_username}"
-      password                                              = "${var.vm_password}"
-      host                                                  = "${var.pip-ansible}"
+      type     = "ssh"
+      user     = "${var.vm_username}"
+      password = "${var.vm_password}"
+      host     = "${var.pip-ansible}"
     }
   }
 }
